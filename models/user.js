f1feaed9d1c82679159db824cc0ff030d0d56358
HarrisonAtiEdge <151602528+HarrisonAtiEdge@users.noreply.github.com>
2026-09-23 13:42:44 +0500

    Delete models directory

--- models/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = mongoose.model("Admins", {
-    name: { type: String, required: true, },
-    email: { type: String, required: true, },
-    password: { type: String, required: true, },
-    otp: Number
-}, 'admins');
